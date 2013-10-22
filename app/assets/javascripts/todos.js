5f65ad09111a52a9aec20534657f330f1b68a9fc
Razix <uscreamrazix@gmail.com>
2013-10-22 11:50:15 +0300

    make ajax task completion work with pagination

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,11 +1,20 @@
-jQuery.fn.submitOnCheck = function() {
-  this.find('input[type=submit]').remove();
-  this.find('input[type=checkbox]').click(function() {
+// jQuery.fn.submitOnCheck = function() {
+//   this.find('input[type=submit]').remove();
+//   this.find('input[type=checkbox]').click(function() {
+//     $(this).parent('form').submit();
+//   });
+//   return this;
+// }
+
+// $(function() {
+//   $('.edit_todo').submitOnCheck();
+// });
+
+
+$(document).bind("DOMSubtreeModified", function() {
+  $('.edit_todo input[type=submit]').remove();
+  $('.edit_todo input[type=checkbox]').click(function() {
     $(this).parent('form').submit();
   });
-  return this;
-}
-
-$(function() {
-  $('.edit_todo').submitOnCheck();
 });
+
